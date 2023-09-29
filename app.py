15018dde475f3b00cd03cc3d8eb66e541fd163dc
jackson-ribeiro <jacksonquest1@gmail.com>
2023-09-28 22:27:00 -0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "minha-chave-secreta"
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return redirect(url_for("home"))
+
+        return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/home")
+def home():
+    if "user_id" not in session:
+        return redirect(url_for("login"))
+
+    return render_template("home.html")
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+        if not User.query.filter_by(username="aluno").first():
+            new_user = User(
+                username="aluno",
+                password=generate_password_hash("unit", method="sha256"),
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+    app.run(debug=True)
